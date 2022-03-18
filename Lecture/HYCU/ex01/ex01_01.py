7f35ec0e647067560b5acf5cb0713a6dc8129cc4
Minjae Jay Kim <exploit017@gmail.com>
2022-03-18 22:42:43 +0900

    HYCU - ex01_01

---
+++ Lecture/HYCU/ex01/ex01_01.py
@@ -0,0 +1,10 @@
+def Sum(s, e):
+  sum = 0
+  for i in range(s, e + 1):
+    sum += i
+
+  return sum
+
+print("sum = %d" % Sum(1, 10))
+print("sum = %d" % Sum(5, 10))
+print("sum = %d" % Sum(10, 20))
