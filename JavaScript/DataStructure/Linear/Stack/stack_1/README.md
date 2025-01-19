f1e5a2726357c7c884c4c900ba30c114b86a5335
minjai100111 <minjai100111@gmail.com>
2025-01-20 00:34:50 +0900

    remove useless JS files

--- JavaScript/DataStructure/Linear/Stack/stack_1/README.md
+++
@@ -1,7 +0,0 @@
-Stack 기본 스팩
-
-class Node {
-data;
-next;
-prev?; // if Circular
-}
--- JavaScript/Tip/no-shift-method.js
+++
@@ -1,9 +0,0 @@
-/**
- * JS 배열에서 shift() 메소드는 선형시간(O(n))이 소요된다
- *
- */
-
-const queue = [1, 2, 3];
-queue.push(4);
-const value = queue.shift(); // ❌: O(n)
-console.log(value);
