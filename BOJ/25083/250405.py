9cfbc3fb08ffaac55f1a5856aca073d10459cfd4
minjai100111 <minjai100111@gmail.com>
2025-04-05 12:17:04 +0900

    feat: boj 25083 250405 python

---
+++ BOJ/25083/250405.py
@@ -0,0 +1,9 @@
+print("         ,r'\"7")
+print("r`-_   ,'  ,/")
+print(" \\. \". L_r'")
+print("   `~\\/")
+print("      |")
+print("      |")
+
+# Fail: 출력 형식이 잘못되었습니다 x2
+# escape 문자, 공백을 잘 눈여겨보자..
