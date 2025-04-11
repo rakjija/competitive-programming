7ed8df11b89ade3d06666f36b1ba1abdeeebe621
Minjae Kim <minjai100111@gmail.com>
2025-04-12 07:39:53 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,18 @@
+import sys
+
+S = sys.stdin.readline().strip()
+
+if S == "M":
+    print("MatKor")
+
+if S == "W":
+    print("WiCys")
+
+if S == "C":
+    print("CyKor")
+
+if S == "A":
+    print("AlKor")
+
+if S == "$":
+    print("$clear")
