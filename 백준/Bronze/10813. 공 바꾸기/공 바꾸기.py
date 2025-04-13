ddb91492612016bbfedad3f93c98f582cdfcea70
Minjae Kim <minjai100111@gmail.com>
2025-04-14 07:21:42 +0900

    [Bronze II] Title: 공 바꾸기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,13 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+
+basket = [i + 1 for i in range(N)]
+
+for _ in range(M):
+    i, j = map(int, sys.stdin.readline().split())
+    temp = basket[i - 1]
+    basket[i - 1] = basket[j - 1]
+    basket[j - 1] = temp
+
+print(*basket)
