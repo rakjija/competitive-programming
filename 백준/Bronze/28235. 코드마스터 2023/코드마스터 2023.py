84c2d8167dc4d689424466b700f85596db9fbbc9
Minjae Kim <minjai100111@gmail.com>
2025-04-16 09:14:28 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,12 @@
+import sys
+
+cheer = {
+    "SONGDO": "HIGHSCHOOL",
+    "CODE": "MASTER",
+    "2023": "0611",
+    "ALGORITHM": "CONTEST",
+}
+
+S = sys.stdin.readline().strip()
+
+print(cheer[S])
