2b0edf32739fb0b76f9c0741e18e08e2a926d023
Minjae Kim <minjai100111@gmail.com>
2025-04-19 21:19:25 +0900

    [Bronze V] Title: 사칙연산, Time: 4 ms, Memory: 864 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var A, B int
+	fmt.Scanf("%d %d", &A, &B)
+
+	fmt.Println(A + B)
+	fmt.Println(A - B)
+	fmt.Println(A * B)
+	fmt.Println(A / B)
+	fmt.Println(A % B)
+}
