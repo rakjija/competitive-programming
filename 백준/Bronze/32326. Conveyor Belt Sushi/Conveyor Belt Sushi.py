a166602f907425640f1863e59d7e92eadbd0fdca
Minjae Kim <minjai100111@gmail.com>
2025-04-23 22:08:46 +0900

    [Bronze V] Title: Conveyor Belt Sushi, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/32326. Conveyor Belt Sushi/Conveyor Belt Sushi.py
@@ -0,0 +1,7 @@
+import sys
+
+R = int(sys.stdin.readline().rstrip("\n"))
+G = int(sys.stdin.readline().rstrip("\n"))
+B = int(sys.stdin.readline().rstrip("\n"))
+
+print((R * 3) + (G * 4) + (B * 5))
