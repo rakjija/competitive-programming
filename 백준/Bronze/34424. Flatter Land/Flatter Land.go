1fa61f19d473cdfda0dbdcb9f62c02704a153703
Minjae Kim <minjai100111@gmail.com>
2025-09-20 20:33:23 +0900

    [Bronze V] Title: Flatter Land, Time: 4 ms, Memory: 976 KB -BaekjoonHub

---
+++ 백준/Bronze/34424. Flatter Land/Flatter Land.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var n int
+	fmt.Fscan(in, &n)
+
+	var x int
+	fmt.Fscan(in, &x)
+
+	fmt.Fprintln(out, (n-1)*x)
+}
