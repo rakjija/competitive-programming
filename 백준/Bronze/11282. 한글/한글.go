4d057eca7fe8e38d064f412a000d3ed906649735
Minjae Kim <minjai100111@gmail.com>
2025-10-20 09:10:43 +0900

    [Bronze IV] Title: 한글, Time: 4 ms, Memory: 1004 KB -BaekjoonHub

---
+++ 백준/Bronze/11282. 한글/한글.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var n int
+	fmt.Fscan(in, &n)
+
+	fmt.Fprintf(out, "%c", '가'+n-1)
+}
