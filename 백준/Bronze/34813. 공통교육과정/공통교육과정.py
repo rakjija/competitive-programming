4d3157e0f3b8b2b494b6041692d860c96aa0cd61
Minjae Kim <minjai100111@gmail.com>
2025-12-01 09:14:32 +0900

    [Bronze V] Title: 공통교육과정, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/34813. 공통교육과정/공통교육과정.py
@@ -0,0 +1,12 @@
+import sys
+
+n = sys.stdin.readline().strip()
+
+if n[0] == "F":
+    print("Foundation")
+if n[0] == "C":
+    print("Claves")
+if n[0] == "V":
+    print("Veritas")
+if n[0] == "E":
+    print("Exploration")
